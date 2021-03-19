6bc65e052a91824afcb1bccb1d68c3806c63c906
Pranav Mathur <pranavmathur99@gmail.com>
2021-03-18 20:34:32 -0700

    Add Node skeleton functions

--- tree.h
+++ tree.h
@@ -3,4 +3,32 @@
 
 using namespace std;
 
+class Node {
+private:
+    int *keys;
+    Node *children;
+    int size;
+    bool leaf;
+public:
+    Node(int size);
+    ~Node();
+    bool has_key(int key);
+    void insert_key(int key);
+    Node *get_child(int index);
+    void *set_child(int index);
+    
+    bool is_full();
+    bool is_leaf();
+};
+
+class BTree {
+private:
+    Node *root;
+public:
+    BTree();
+    ~BTree();
+    
+    void insert(int key);
+};
+
 #endif
